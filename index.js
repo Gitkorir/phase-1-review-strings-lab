3c08cb8578fe328ace018bc4ccea2c5baee8298b
Arnold Korir <arnoldkorir201@gmail.com>
2025-03-04 17:35:06 +0300

    declare the variables

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+let currentUser = "P-Diddy Kasongo";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+let shortGreeting = `Welcome, ${currentUser[0]} !`;
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = {
+    currentUser,
+    welcomeMessage,
+    excitedWelcomeMessage,
+    shortGreeting,
+  };
+}
